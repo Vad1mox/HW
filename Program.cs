cded53ac5c00d2f9bf4f62941e0f357ab184c85e
Vadimox <rucavnarucave@mail.ru>
2023-12-27 23:12:19 +0300

    первое задание

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello world");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int A = 161;
+
+        int mult7 = A % 7;
+        int mult23 = A % 23;
+
+        if (mult7 == 0)
+        {
+            if (mult23 == 0)
+            {
+                Console.WriteLine("Кратно");
+            }
+        }
+    }
+}
