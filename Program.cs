260c109c5d10db8c2920c44d6f99a060aae82428
Vadimox <rucavnarucave@mail.ru>
2023-12-28 18:17:44 +0300

    Закончил 2е задание

--- Program.cs
+++ Program.cs
@@ -15,12 +15,44 @@ internal class Program
             }
             else
             {
-                Console.WriteLine("Не_кратно")
+                Console.WriteLine("Не_кратно");
             }
         }
         else
         {
-            Console.WriteLine("Не_кратно")
+            Console.WriteLine("Не_кратно");
+        }
+    }
+}
+
+internal class Program2
+{
+    private static void Main2(string[] args)
+   {
+         int X = 2;
+        int Y = -3;
+
+        if (X < 0)
+        {
+            if (Y < 0)
+            {
+                Console.WriteLine(3);
+            }
+            else
+            {
+                Console.WriteLine(2);
+            }
+         }
+     else
+     {
+            if (Y < 0)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(4);
+            }
         }
     }
 }
